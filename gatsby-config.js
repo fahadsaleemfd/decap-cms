14e3385a7eb9c93c954160ae2aeede7247e4b24a
fahadsaleemfd <fahadsaleem164@outlook.com>
2024-10-05 14:29:57 +0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,10 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-google-gtag"]
+	siteMetadata: {
+		title: `My Gatsby Site`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: ["gatsby-plugin-decap-cms"],
 };
+
